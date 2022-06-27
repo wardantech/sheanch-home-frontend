3ae484b1693a53fd264d20a322137ad2725e5793
shahriarshaon19 <shahriarshaon1993@gmail.com>
2022-06-27 12:46:14 +0600

    OTP Veryfication Page Design

--- components/frontend/Topbar.vue
+++ components/frontend/Topbar.vue
@@ -8,7 +8,7 @@
                             <li>
                                 <a href="#">
                                     <img src="https://flagicons.lipis.dev/flags/4x3/bd.svg"/>
-                                    <span>Bangla</span>
+                                    <span>Bangladesh</span>
                                 </a>
                             </li>
                         </ul>
@@ -21,13 +21,13 @@
                                 Wishlist (0)
                             </a>
                         </div>
-                        <div class="ms-3 text-white">
-                            <b-dropdown size="sm" text="Lang" class="m-2 top-bar-right-lang">
-                                <b-dropdown-item-button>EN</b-dropdown-item-button>
-                                <b-dropdown-item-button>BN</b-dropdown-item-button>
-                                <b-dropdown-item-button>LN</b-dropdown-item-button>
-                            </b-dropdown>
-                        </div>
+                        <!--<div class="ms-3 text-white">-->
+                            <!--<b-dropdown size="sm" text="Lang" class="m-2 top-bar-right-lang">-->
+                                <!--<b-dropdown-item-button>EN</b-dropdown-item-button>-->
+                                <!--<b-dropdown-item-button>BN</b-dropdown-item-button>-->
+                                <!--<b-dropdown-item-button>LN</b-dropdown-item-button>-->
+                            <!--</b-dropdown>-->
+                        <!--</div>-->
                     </div>
                 </div>
             </b-container>
