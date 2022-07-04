0f32ca92147bd0a2b2f3f7da37a92e763e788b92
shahriarshaon19 <shahriarshaon1993@gmail.com>
2022-07-04 06:56:57 +0600

    frontend Search page

--- components/frontend/Banner.vue
+++ components/frontend/Banner.vue
@@ -1,109 +1,176 @@
 <template>
-    <div>
-        <div class="banner image-cover" style="background: url('https://resido.thesky9.com/storage/banners/banner-1.jpg') no-repeat;">
-            <b-container>
-                <div class="banner-form">
-                    <div class="banner-form-title">
-                        <h1>Find Accessible Homes To Rent</h1>
-                    </div>
+  <div>
+    <div class="banner image-cover"
+         style="background: url('https://resido.thesky9.com/storage/banners/banner-1.jpg') no-repeat;">
+      <b-container>
+        <div class="banner-form">
+          <div class="banner-form-title">
+            <h1>Find Accessible Homes To Rent</h1>
+          </div>
 
-                    <form action="" method="POST">
-                        <div class="banner-form-content">
-                            <b-row>
-                                <b-col lg="12" md="12" sm="12">
-                                    <b-form-group>
-                                        <div class="search-location">
-                                            <b-form-input id="location" type="text" placeholder="Search for a location" required></b-form-input>
-                                            <b-img src="https://resido.thesky9.com/themes/resido/img/pin.svg" width="18"></b-img>
-                                        </div>
-                                    </b-form-group>
-                                </b-col>
-                            </b-row>
+          <form action="" method="POST">
+            <div class="banner-form-content">
+              <b-row class="my-4">
+                <b-col lg="12" md="12" sm="12">
+                  <b-form-radio-group
+                    id="btn-radios-2"
+                    :options="options"
+                    :aria-describedby="ariaDescribedby"
+                    button-variant="outline-primary"
+                    size="lg"
+                    name="radio-btn-outline"
+                    buttons
+                    class="btn-block"
+                  ></b-form-radio-group>
+                </b-col>
+              </b-row>
 
-                            <b-row>
-                                <b-col lg="6" md="6" sm="6">
-                                    <b-form-group label="Min Price" label-for="min_price">
-                                        <select class="form-control" id="min_price">
-                                            <option>No Min</option>
-                                            <option>500</option>
-                                            <option>1000</option>
-                                            <option>2000</option>
-                                            <option>50000</option>
-                                            <option>10000</option>
-                                        </select>
-                                    </b-form-group>
-                                </b-col>
+              <b-row>
+                <b-col lg="6" md="6" sm="6">
+                  <b-form-group label="Min Price" label-for="min_price">
+                    <select class="form-control" id="min_price">
+                      <option>No Min</option>
+                      <option>500</option>
+                      <option>1000</option>
+                      <option>2000</option>
+                      <option>50000</option>
+                      <option>10000</option>
+                    </select>
+                  </b-form-group>
+                </b-col>
 
-                                <b-col lg="6" md="6" sm="6">
-                                    <b-form-group label="Max Price" label-for="max_price">
-                                        <select class="form-control" id="max_price">
-                                            <option>No Max</option>
-                                            <option>1000</option>
-                                            <option>2000</option>
-                                            <option>5000</option>
-                                            <option>10000</option>
-                                            <option>20000</option>
-                                        </select>
-                                    </b-form-group>
-                                </b-col>
-                            </b-row>
+                <b-col lg="6" md="6" sm="6">
+                  <b-form-group label="Max Price" label-for="max_price">
+                    <select class="form-control" id="max_price">
+                      <option>No Max</option>
+                      <option>1000</option>
+                      <option>2000</option>
+                      <option>5000</option>
+                      <option>10000</option>
+                      <option>20000</option>
+                    </select>
+                  </b-form-group>
+                </b-col>
+              </b-row>
 
-                            <b-row>
-                                <b-col lg="6" md="6" sm="6">
-                                    <b-form-group label="Property Type" label-for="property_type">
-                                        <select class="form-control" id="property_type">
-                                            <option>Category</option>
-                                            <option>Apartment</option>
-                                            <option>Villa</option>
-                                            <option>Conda</option>
-                                            <option>House</option>
-                                        </select>
-                                    </b-form-group>
-                                </b-col>
+              <b-row>
+                <b-col lg="6" md="6" sm="6">
+                  <b-form-group label="Property Type" label-for="property_type">
+                    <select v-model="form.property_type_id"
+                            class="form-control custom-input-control">
+                      <option value="">Select</option>
+                      <option v-for="(type, i) in propertyTypes" :key="i" :value="type.id">
+                        {{ type.name }}
+                      </option>
+                    </select>
+                  </b-form-group>
+                </b-col>
 
-                                <b-col lg="6" md="6" sm="6">
-                                    <b-form-group label="Bed Rooms" label-for="bed_rooms">
-                                        <select class="form-control" id="bed_rooms">
-                                            <option>Bedroom</option>
-                                            <option>1</option>
-                                            <option>2</option>
-                                            <option>3</option>
-                                            <option>4</option>
-                                            <option>5</option>
-                                        </select>
-                                    </b-form-group>
-                                </b-col>
-                            </b-row>
+                <b-col lg="6" md="6" sm="6">
+                  <b-form-group label="Unit Type" label-for="unit">
+                    <select class="form-control" id="unit">
+                      <option>Select</option>
+                      <option>Apartment</option>
+                      <option>Room</option>
+                      <option>Plaza</option>
+                    </select>
+                  </b-form-group>
+                </b-col>
+              </b-row>
 
-                            <b-row>
-                                <b-col>
-                                    <b-form-group label="Property Location" label-for="property_location">
-                                        <select class="form-control" id="property_location">
-                                            <option>City</option>
-                                            <option>Dhaka</option>
-                                            <option>Rajshahi</option>
-                                            <option>Pabna</option>
-                                            <option>Bagha</option>
-                                            <option>Bogra</option>
-                                        </select>
-                                    </b-form-group>
-                                </b-col>
-                            </b-row>
-                        </div>
-                        <div class="banner-form-action">
-                            <b-button variant="primary banner-form-action-button">Search Result</b-button>
-                        </div>
-                    </form>
-                </div>
-            </b-container>
-      </div>
+              <b-row>
+                <b-col md="6" sm="12">
+                  <b-form-group label="Select divisions" label-for="division_id">
+                    <select @change="getDistricts(form.division_id)" v-model="form.division_id" id=""
+                            class="form-control custom-input-control">
+                      <option value="">Select</option>
+                      <option v-for="(division, i) in divisions" :value="division.id" :key="i">
+                        {{ division.name }}
+                      </option>
+                    </select>
+                  </b-form-group>
+                </b-col>
+
+                <b-col md="6" sm="12">
+                  <b-form-group label="Select district" label-for="district_id">
+                    <select @change="getThanas(form.district_id)" v-model="form.district_id" id=""
+                            class="form-control custom-input-control">
+                      <option value="">Select</option>
+                      <option v-for="(district, i) in districts" :value="district.id" :key="i">
+                        {{ district.name }}
+                      </option>
+                    </select>
+                  </b-form-group>
+                </b-col>
+              </b-row>
+
+              <b-row>
+                <b-col md="12">
+                  <b-form-group label="Select Thana" label-for="thana_id">
+                    <select v-model="form.thana_id" class="form-control custom-input-control">
+                      <option value="">Select</option>
+                      <option v-for="(thana, i) in thanas" :value="thana.id" :key="i">
+                        {{ thana.name }}
+                      </option>
+                    </select>
+                  </b-form-group>
+                </b-col>
+              </b-row>
+            </div>
+            <div class="banner-form-action">
+              <b-button variant="primary banner-form-action-button">Search Result</b-button>
+            </div>
+          </form>
+        </div>
+      </b-container>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Banner"
+  export default {
+    name: "Banner",
+    data() {
+      return {
+        options: [
+          {text: 'For Sale', value: '1'},
+          {text: 'To Rent', value: '2'}
+        ],
+        form: {
+          property_type_id: '',
+          division_id: '',
+          district_id: '',
+          thana_id: '',
+        },
+        units: '',
+        thanas: '',
+        divisions: '',
+        districts: '',
+        propertyTypes: ''
+      }
+    },
+
+    async created() {
+      const propertyTypes = await this.$axios.$get('property/get-property-type');
+      this.propertyTypes = propertyTypes.data;
+
+      const divisions = await this.$axios.$get('settings/divisions');
+      this.divisions = divisions.data;
+    },
+
+    methods: {
+      async getDistricts(division_id) {
+        this.thanas = '';
+        let district = await this.$axios.$post('settings/districts', {divisionId: division_id});
+        this.districts = district.data;
+      },
+
+      async getThanas(district_id) {
+        let thanas = await this.$axios.$post('settings/thanas', {districtId: district_id});
+        this.thanas = thanas.data;
+      },
     }
+  }
 </script>
 
 <style scoped>
