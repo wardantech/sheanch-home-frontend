02eab5467e9ac227bf4fa8444efdccb17d825dfd
wardantech <wardan.tech@gmail.comgit config --global user.email wardan.tech@gmail.comgit config --global user.email wardan.tech@gmail.com>
2022-08-17 19:48:46 +0600

    issue solve

--- pages/account/property-deed/tenant.vue
+++ pages/account/property-deed/tenant.vue
@@ -80,7 +80,7 @@
                         <font-awesome-icon icon="fa-solid fa-eye"/>
                       </nuxt-link>
 
-                      <nuxt-link :to="{name:'account-user-id-show-landlord', params: { id: value.landlord_id }}" rel="tooltip"
+                      <nuxt-link :to="{name:'account-profile-id-show-landlord', params: { id: value.landlord_id }}" rel="tooltip"
                                  class="btn btn-sm btn-warning btn-simple"
                                  title="View landlord">
                         <font-awesome-icon icon="fa-solid fa-eye"/>
