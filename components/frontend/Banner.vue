1f0a198ee77d40b2b05ac344a114b8658e744762
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-06-01 17:39:19 +0600

    home page create

--- components/frontend/Banner.vue
+++ components/frontend/Banner.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div class="banner image-cover" :style="bannerImage">
+    <div class="banner image-cover" :style="`background: url(${url+banner.banner_image}) no-repeat`">
       <b-container>
-        <div class="banner-content">
-          <h1 class="banner-title">Welcome to <br />Sheanch Engineering Ltd.</h1>
-          <p class="banner-text">We are committed to providing you with a safe home.</p>
+        <div class="banner-content text-center">
+          <h1 class="banner-title">{{ banner.banner_title }}</h1>
+          <p class="banner-text">{{ banner.caption }}</p>
         </div>
       </b-container>
     </div>
@@ -14,18 +14,16 @@
 <script>
   export default {
     name: "Banner",
+    props: {
+      banner: Object
+    },
     data() {
       return {
-        bannerImage: ''
+        url: ''
       }
     },
     created() {
-      const banner = this.$store.getters['frontend-data/getBanner'];
-      this.bannerImage = "background: url(" + banner + ") no-repeat";
+      this.url = process.env.APP_ROOT_IMG_URL;
     }
   }
 </script>
-
-<style scoped>
-
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Start Banner -->
-    <Banner />
+    <Banner :banner="banner"/>
     <!-- End Banner -->
 
     <!-- Search -->
@@ -9,11 +9,11 @@
     <!-- /. Search -->
 
     <!-- Start place section -->
-    <Property />
+    <Property v-if="banner.is_properties"/>
     <!-- End place section -->
 
     <!-- Start how work section -->
-    <Work />
+    <Work v-if="banner.is_work"/>
     <!-- End how work section -->
 
     <!-- Start Location -->
@@ -21,21 +21,35 @@
     <!-- End Location -->
 
     <!-- Start newsletter -->
-    <Newsletter />
+    <Newsletter v-if="banner.is_newslater" />
     <!-- End newsletter -->
   </div>
 </template>
 
 <script>
-  import Banner from "@/components/frontend/Banner";
-  import Work from "@/components/frontend/Work";
-  import Property from "@/components/frontend/Property";
-  import Newsletter from "@/components/frontend/Newsletter";
-  import Search from "@/components/frontend/Search";
+import Banner from "@/components/frontend/Banner";
+import Work from "@/components/frontend/Work";
+import Property from "@/components/frontend/Property";
+import Newsletter from "@/components/frontend/Newsletter";
+import Search from "@/components/frontend/Search";
 
-  export default {
-    name: 'IndexPage',
-    auth: false,
-    components: { Newsletter, Property, Work, Banner, Search }
-  }
+export default {
+  name: 'IndexPage',
+  auth: false,
+  components: { Newsletter, Property, Work, Banner, Search },
+  data() {
+    return {
+      banner: {}
+    }
+  },
+  async created() {
+    await this.$axios.$post('get-home-page')
+      .then(response => {
+        this.banner = response.homeData;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
+}
 </script>
